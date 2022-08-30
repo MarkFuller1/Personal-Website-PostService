89fad7112b9eedb75f126a8a797d66679f2191f1
Mark Fuller <Mark_Fuller1@baylor.edu>
2022-08-29 20:12:26 -0700

    group by ip lat lon

--- src/main/java/my/site/ithoughtilearned/controller/GeoDataController.java
+++ src/main/java/my/site/ithoughtilearned/controller/GeoDataController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/geoData")
@@ -17,7 +19,7 @@ public class GeoDataController {
     GeoService geoService;
 
     @GetMapping
-    public List<GeoData> getAllGeoData(){
-       return geoService.getAll();
+    public Map<String, List<GeoData>> getAllGeoData() {
+        return geoService.getAll().stream().collect(Collectors.groupingBy(geo -> geo.ip + "|" + geo.lat + "|" + geo.lon));
     }
 }
